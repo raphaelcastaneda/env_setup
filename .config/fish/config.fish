293b62ecd9385d159232fcb6eddd546efc8117e4
Raphael Castaneda <racastaneda@tesla.com>
2024-03-13 12:04:05 -0700

    add config dir for linking

---
+++ .config/fish/config.fish
@@ -0,0 +1,2 @@
+
+source /Users/racastaneda/.docker/init-fish.sh || true # Added by Docker Desktop
---
+++ .config/nvim/lua/rainbow_delimiters.lua
@@ -0,0 +1,72 @@
+-- This module contains a number of default definitions
+local rainbow_delimiters = require 'rainbow-delimiters'
+
+vim.g.rainbow_delimiters = {
+  strategy = {
+    [''] = rainbow_delimiters.strategy['global'],
+    vim = rainbow_delimiters.strategy['local'],
+  },
+  query = {
+    [''] = 'rainbow-delimiters',
+    lua = 'rainbow-blocks',
+  },
+  highlight = {
+    'RainbowRed',
+    'RainbowYellow',
+    'RainbowBlue',
+    'RainbowOrange',
+    'RainbowGreen',
+    'RainbowViolet',
+    'RainbowCyan',
+  },
+}
+
+
+-- Integration with indent-blankline
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+require("ibl").setup {
+  -- whitespace = {
+  --   highlight = highlight,
+  --   remove_blankline_trail = false,
+  -- },
+  scope = {
+    highlight = highlight,
+    show_start = false,
+    show_end = false,
+    include = {
+      node_type = {
+        lua = { "*" },
+        json = { "*" },
+        bash = { "block" },
+      }
+
+    }
+  },
+}
+
+hooks.register(
+  hooks.type.SCOPE_HIGHLIGHT,
+  hooks.builtin.scope_highlight_from_extmark
+  )
