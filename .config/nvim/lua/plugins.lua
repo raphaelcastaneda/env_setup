d4bb692b77628bea8825a359949b6dfb1260d0d4
raphaelcastaneda <raphael.castaneda@gmail.com>
2024-07-02 15:29:48 -0700

    lint formatting

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -207,8 +207,10 @@ return require('packer').startup(function(use)
       vim.g.codeium_disable_bindings = 1
       vim.keymap.set('n', '<A-j>', function() return vim.fn['codeium#Chat']() end, { expr = true, silent = true })
       vim.keymap.set('i', '<A-l>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-      vim.keymap.set('i', '<A-j>', function() return vim.fn['codeium#CycleOrComplete']() end, { expr = true, silent = true })
-      vim.keymap.set('i', '<A-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<A-j>', function() return vim.fn['codeium#CycleOrComplete']() end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<A-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        { expr = true, silent = true })
       vim.keymap.set('i', '<A-h>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
     end
   }
